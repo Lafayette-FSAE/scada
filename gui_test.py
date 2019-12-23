540560cc0026c0c3c01fc9b40f55a2cebbea75c7
Jon Abel <jabel88keys@gmail.com>
2019-12-23 14:20:08 -0500

    Reading in drive state, sloop nodes, and sensor names from config file and displaying

--- gui_test.py
+++ gui_test.py
@@ -1,32 +1,42 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+import yaml
+
+config = {}
+with open("config.yaml", 'r') as stream:
+	try:
+		config = yaml.safe_load(stream)
+	except yaml.YAMLError as exc:
+		print(exc)
+
+# for group in config['sensors']:
+# 	print(group)
+# 	for sensor in config['sensors'][group]:
+# 		print(sensor)
 
 root = tk.Tk()
 root.geometry('1280x720')
 root.title('pySCADA GUI')
 
-sensorInfoFrame = tk.Frame(root, relief=tk.RIDGE, borderwidth=4)
-driveFSMFrame = tk.Frame(root, relief=tk.RIDGE, borderwidth=4)
-sloopFrame = tk.Frame(root, relief=tk.RIDGE, borderwidth=4)
-
-sensorInfoFrame.pack(fill=tk.BOTH, expand=True)
-sensorInfoFrameText = tk.Label(sensorInfoFrame, text='Sensor Info')
-sensorInfoFrameText.pack(padx=5, pady=5)
-sensorGroups = ['GLV', 'TSI', 'Cooling', 'Motor Controller', 'Pack 1', 'Pack 2']
+sensorInfoFrame = tk.LabelFrame(root, text='Sensors', relief=tk.RIDGE, borderwidth=3)
+sensorInfoFrame.pack(padx=2, pady=2, fill=tk.BOTH, expand=True)
+sensorGroups = config['sensors']
 for node in sensorGroups:
-	tk.Label(sensorInfoFrame, text=node, relief=tk.GROOVE, height=2).pack(side='left', padx=5, pady=10, fill=tk.X, expand=True)
+	frame = tk.LabelFrame(sensorInfoFrame, text=node, relief=tk.RIDGE, borderwidth=2)
+	frame.pack(side='left', padx=10, pady=10, fill=tk.BOTH, expand=True)
+	for sensor in sensorGroups[node]:
+		label = tk.Label(frame, text=sensor)
+		label.pack(padx=5, pady=5)
 
-driveFSMFrame.pack(fill=tk.BOTH, expand=False)
-driveFSMFrameText = tk.Label(driveFSMFrame, text='Drive State FSM')
-driveFSMFrameText.pack(padx=5, pady=5)
-driveFSMNodes = ['GLV Off', 'GLV On', 'Precharge', 'TS Energized', 'Ready to Drive', 'AMS Fault', 'IMD Fault', 'Brake Overtravel']
+driveFSMFrame = tk.LabelFrame(root, text='Drive State FSM', relief=tk.RIDGE, borderwidth=3)
+driveFSMFrame.pack(padx=2, pady=2, fill=tk.BOTH, expand=False)
+driveFSMNodes = config['drive_states']
 for node in driveFSMNodes:
 	tk.Label(driveFSMFrame, text=node, relief=tk.GROOVE, bg='dodger blue', height=2).pack(side='left', padx=5, pady=10, fill=tk.BOTH, expand=True)
 
-sloopFrame.pack(fill=tk.BOTH, expand=False)
-sloopFrameText = tk.Label(sloopFrame, text='Safety Loop State')
-sloopFrameText.pack(padx=5, pady=5)
-sloopNodes = ['TSI, S1', 'TSI, S2', 'Pack 1, S1', 'Pack 1, S2', 'Pack 2, S1', 'Pack 2, S2']
+sloopFrame = tk.LabelFrame(root, text='Safety Loop State', relief=tk.RIDGE, borderwidth=3)
+sloopFrame.pack(padx=2, pady=2, fill=tk.BOTH, expand=False)
+sloopNodes = config['sloop_nodes']
 for node in sloopNodes:
 	tk.Label(sloopFrame, text=node, relief=tk.GROOVE, bg='green2', height=2).pack(side='left', padx=5, pady=10, fill=tk.BOTH, expand=True)
 
