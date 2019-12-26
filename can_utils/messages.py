25a654fac2e8c633770f502e24d37c203a8e98a7
winiarcc <winiarcc@lafayette.edu>
2019-12-26 17:41:17 -0500

    Cleaned up CAN utils and renamed methods

--- can_utils/messages.py
+++ can_utils/messages.py
@@ -1,35 +1,77 @@
 import can
 
-def sdo_read(node_id, index, subindex):
-	cob_id = 0x600 + node_id
-	data = [0x00] + index + subindex + [0x00, 0x00, 0x00, 0x00]
+FUNCTIONS = {
+	# Network Management
+	0x000: "NMT",
+
+	# Sync
+	0x080: "SYNC",
+
+	# PDOs 1-4
+	0x180: 'PDO',
+	0x280: 'PDO2',
+	0x380: 'PDO3',
+	0x480: 'PDO4',
+
+	# SDO Write
+	0x580: 'SDO-WRITE',
+
+	# SDO Read
+	0x600: 'SDO-READ',
+}
+
+def get_function(code):
+	return FUNCTIONS.get(code, lambda: None)
+
+def get_code(function):
+	inv_functions = {function: code for code, function in FUNCTIONS.items()}
+
+	return hex(inv_functions.get(function))
+
+def can_message(function, node_id, data):
 	
-	message = can.Message(arbitration_id=cob_id, data=data)
+	function_code = get_code(function)
+	cob_id = function_code + node_id
+
+	message = can.Message(arbitration_id = cob_id, data=data)
 	message.is_extended_id = False
 
 	return message
 
-def sdo_write(node_id, index, subindex, value):
-	pass
+def pdo(node_id, data, pdo_number = 1):
+	#http://www.byteme.org.uk/canopenparent/canopen/pdo-process-data-objects-canopen/
 
-def transmit_pdo(node_id, data, pdo_number = 1):
+	return can_message('PDO', node_id, data)
 
-	"""
-	Returns a transmit PDO (Process Data Object) 
-	with the given node_id and data
+def sdo_read(node_id, index, subindex=0xFF):
+	#http://www.byteme.org.uk/canopenparent/canopen/sdo-service-data-objects-canopen/
+	# TODO: use command byte
+
+	command_byte = 0x00 
+	data = [command_byte] + index + [subindex] + [0x00, 0x00, 0x00, 0x00]
+
+	return can_message('SDO-READ', node_id, data)
+
+def sdo_write(node_id, index, subindex=0xFF, value):
+	command_byte = 0x00
+	data = [command_byte] + index + [subindex] + [0x00, 0x00, 0x00, hex(value)]
+
+	return can_message('SDO-WRITE', node_id, data)
 
 
-	http://www.byteme.org.uk/canopenparent/canopen/pdo-process-data-objects-canopen/
+def get_info(message):
+	# get last (hex) digit of cob_id
+	node_id = cob_id % 16
+	function_id = cob_id - node_id
 
-	"""
+	function = get_function(function_id)
 
-	function_id = hex( (pdo_number * 16 * 16) + (8 * 16))
+	return (function, node_id)
 
-	print(transmit_pdo)
 
-	cob_id = function_id + node_id
 
-	message = can.Message(arbitration_id=cob_id, data=data)
-	message.is_extended_id = False
 
-	return message
+
+
+
+
