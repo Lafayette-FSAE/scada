13411998b5981bcb984643c5e9d1b85ee387ee42
Jon Abel <jabel88keys@gmail.com>
2019-12-29 23:21:12 -0500

    Moved sensor updates to a separate function, and made it so they are only updated when a SYNC message arrives

--- main.py
+++ main.py
@@ -61,7 +61,6 @@ master_bus.send_periodic(sync, .1)
 # \/  GUI Integration  \/
 # \/				   \/
 
-
 from scada_gui import SCADA_GUI
 from time import strftime
 
@@ -70,27 +69,30 @@ scada_logger.set_text_window(app.scadaLogScrolledText)
 
 app.set_value('GLV', 'Voltage', '24 V') # Test changing a value
 
-test_value = 10
-
-class GUIListener(can.Listener):
-	def __init__(self, node_id, gui):
-		self.node_id = node_id
-		self.gui = gui
-
-	def on_message_received(self, msg):
-		global test_value
-		function, node_id = can_utils.messages.get_info(msg)
-		if function == 'PDO' and node_id == 3:
-			# test_value = int(msg.data[4])
-			pass
-
-guiListener = GUIListener(node_id=6, gui=app)
-notifier.add_listener(guiListener)
-
-while app.running:
-
+def update_sensors():
 	app.set_value('TSI', 'HV Current', '{} A'.format(can_utils.data_cache.get('TSI', 'CURRENT')))
 	app.set_value('TSI', 'High Voltage', '{} hundred W'.format(can_utils.data_cache.get('SCADA', 'TS_POWER')))
+
+class GUIListener(can.Listener):
+	def __init__(self, node_id):
+		self.node_id = node_id
+
+	def on_message_received(self, msg):
+		global needSensorUpdate
+
+		function, node_id = can_utils.messages.get_info(msg)
+		
+		if function == 'SYNC':
+			needSensorUpdate = True
+
+guiListener = GUIListener(node_id=6)
+notifier.add_listener(guiListener)
+
+needSensorUpdate = False;
+while app.running:
+	if needSensorUpdate:
+		update_sensors()
+		needSensorUpdate = False;
 	app.timeValue.set(strftime('%D  %I:%M:%S %p'))
 	app.update_idletasks()
 	app.update()
