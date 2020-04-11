409b8c5924a4ffdfb37efdcebb15a0ecbfdbec17
Connor Winiarczyk <connorwiniarczyk@gmail.com>
2020-04-11 17:05:04 -0400

    major refactor of calibrator.py and calibration_utils.py

--- calibrator/calibration_utils.py
+++ calibrator/calibration_utils.py
@@ -1,62 +1,37 @@
-import redis
-data_redis = redis.Redis(host='localhost', port=6379, db=0)
 
+""" 
+A dictionary containing a list of all calibration functions. functions are
+indexed by the name of their output, eg: "pack1:voltage" for the voltage
+measured by pack1 and are stored as a tuple of the function itself, and a 
+list of the names of its arguments.
+
+Output strings and argument strings both represent keys in the redis database
+
+For example, a calibration function for calculating power from the tsi 
+current and voltage would be stored as:
+
+__calibration_functions['tsi:power'] = (<function>, ["tsi:voltage", "tsi:current"])
+"""
 __calibration_functions = {}
 
 # Decorator that adds a function to the list of calibration functions
-# with the given target and arguments
-def cal_function(target, requires):
-
+# with the given output and arguments
+def cal_function(output, arguments):
 	def inner(function):
-		__calibration_functions[target] = (function, requires)
+		__calibration_functions[output] = (function, arguments)
 		return function
 
 	return inner
 
-def targets():
+""" Helper Functions for pulling data from __calibration_functions"""
+
+def get_function_names():
 	return list(__calibration_functions.keys())
 
-def process(target):
-	"""
-	Takes a target, looks up the necessary arguments from 
-	the bus_data dictionary, and excecutes the associated
-	calibration function
+def get_function(output):
+    function, arguments = __calibration_functions[output]
+    return function
 
-	returns a tuple of (error, result)
-
-	"""
-
-	try:
-		function, requires = __calibration_functions[target]
-	except:
-		message = "no calibration function defined for target: {}".format(target)
-		err = Exception(message)
-		return (err, None)
-
-	arguments = []
-
-	for key in requires:
-
-		if type(key) == str:
-			node, name = key.split(': ', 1)
-
-		else:
-			node, name = key
-
-		try:
-			argument_raw = data_redis.get("{node}: {name}".format(node=node, name=name))
-			argument = int(argument_raw)
-			arguments.append(argument)
-		except:
-			message = "could not find key '{}' required for target '{}'".format(key, target)
-			err = Exception(message)
-			return (err, None)
-
-	try:
-		result = function(arguments)
-	except Exception as error:
-		return (error, None)
-		# pass
-		# return (Exception('Error in cal_function: {} \n {}'.format(target, err)), None)
-
-	return (None, result)
+def get_arguments(output):
+    function, arguments = __calibration_functions[output]
+    return arguments
--- calibrator/calibrator.py
+++ calibrator/calibrator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys, os
 
-scada_path = '/home/fsae/test'
+scada_path = '/home/fsae/scada'
 
 sys.path.append(scada_path)
 sys.path.append('{path}/calibrator'.format(path = scada_path))
@@ -16,41 +16,39 @@ import user_cal
 import time
 import datetime
 
-# import logging as log
-# logfile = os.environ['SCADA_LOG']
-# log.basicConfig(
-#		level=log.DEBUG,
-#		filename=logfile,
-#		filemode='w',
-#		format='data_calibrator: %(levelname)s - %(message)s - %(asctime)s'
-# )
+# TODO: reintroduce verbose logging
 
+# Configure Redis interface
 data = redis.Redis(host='localhost', port=6379, db=0)
-
 p = data.pubsub()
 p.subscribe('bus_data')
 
+def execute(cal_function):
+	argument_keys = calibration_utils.get_arguments(cal_function)
+	arguments = []
+	for key in argument_keys:
+		value = int(data_redis.get(key))
+		arguments.append(value_int)
+		
+	function = calibration_utils.get_function(cal_function)
+	result = function(arguments)
+	return result
+		
 def update():
 
-	for target in calibration_utils.targets():
-		err, result = calibration_utils.process(target)
-
-		if err:
-			pass
-			#print(f'failed to calibrate "{target}", cal_function failed with message: "{err}"')
+	for cal_function in calibration_utils.get_function_names():
+		try:
+			result = execute(cal_function)
+			data.setex(cal_function, 10, result)	
+		except: 
+			print(f'failed to calibrate "{target}", cal_function failed with message: "{err}"')
 			# log.warning(f'failed to calibrate "{target}", cal_function failed with message: "{err}"')
-		else:
-			# round numbers to nearest 2 decimal points
-			if type(result) in [float, int]:
-				result = round(result, 2)
-
-			data.setex("SCADA: {target}".format(target = target), 10, result)
 
 	# let the other processes know that there is new calculated data
 	data.publish('calculated_data', '')
 
 while True:
-	if p.get_message():
+	while p.get_message():
 		update()
 		
 	time.sleep(0.1)
