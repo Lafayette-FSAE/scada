f0a974c39f2c21c34a15a3a46a591192994bdc71
Connor Winiarczyk <connor@ip-172-26-4-160.ec2.internal>
2020-05-01 21:53:57 +0000

    added psycopg2 to pip3 install sequence

--- logger/logger.py
+++ logger/logger.py
@@ -2,14 +2,12 @@
 
 import sys, os
 
-# scada_path = os.environ['SCADA_PATH']
-sys.path.append('/home/fsae/test')
+sys.path.append('/home/connor/scada/config')
 
 import config
 import time
 import redis
-import sqlite3
-
+import pyscopg2
 
 data = redis.Redis(host='localhost', port=6379, db=0)
 
@@ -17,58 +15,61 @@ p = data.pubsub()
 p.subscribe('bus_data')
 p.subscribe('calculated_data')
 
-# ---------
-# SQlite util function
-def create_connection(path):
-		conn = None
 
-		try:
-				conn = sqlite3.connect(path)
-		except Error as e:
-				print(e)
 
-#------				
 
-# define some sql strings that will get used a lot
-create_table = """
-	create table if not exists {} (
-		id integer primary key,
-		session integer,
-		value real,
-		time datetime default current_timestamp
-	);
-"""
-
-insert_data = """
-	insert into {} (session, value) 
-	values(1, 10.4);
-"""
-
-database = 'database.db'
-def log_data(key, value):
-	conn = sqlite3.connect(database)
-	c = conn.cursor()
-
-	#make sure the table we want to log to exists
-	c.execute(create_table.format(key))
-
-	# append the value to the end of the table
-	c.execute(insert_data.format(key))#, (1, value))
-	conn.commit()
-
-def update(message, value):
-#	print('new data event!, {}'.format(message))
-
-	# get the most recent value of SCADA: TSI-THROTTLE
-	throttle = data.get('SCADA: TSI-THROTTLE') 
-	log_data('throttle', throttle)
-
-	for key in data.keys('SCADA:*'):
-		print(key)
-
-while True:
-	message = p.get_message()
-	if message:
-		update(message['channel'], message['data'])
-	
-	time.sleep(0.1)
+## ---------
+## SQlite util function
+#def create_connection(path):
+#		conn = None
+#
+#		try:
+#				conn = sqlite3.connect(path)
+#		except Error as e:
+#				print(e)
+#
+##------				
+#
+## define some sql strings that will get used a lot
+#create_table = """
+#	create table if not exists {} (
+#		id integer primary key,
+#		session integer,
+#		value real,
+#		time datetime default current_timestamp
+#	);
+#"""
+#
+#insert_data = """
+#	insert into {} (session, value) 
+#	values(1, 10.4);
+#"""
+#
+#database = 'database.db'
+#def log_data(key, value):
+#	conn = sqlite3.connect(database)
+#	c = conn.cursor()
+#
+#	#make sure the table we want to log to exists
+#	c.execute(create_table.format(key))
+#
+#	# append the value to the end of the table
+#	c.execute(insert_data.format(key))#, (1, value))
+#	conn.commit()
+#
+#def update(message, value):
+##	print('new data event!, {}'.format(message))
+#
+#	# get the most recent value of SCADA: TSI-THROTTLE
+#	throttle = data.get('SCADA: TSI-THROTTLE') 
+#	log_data('throttle', throttle)
+#
+#	for key in data.keys('SCADA:*'):
+#		print(key)
+#
+#while True:
+#	message = p.get_message()
+#	if message:
+#		update(message['channel'], message['data'])
+#	
+#	time.sleep(0.1)
